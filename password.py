ba2b00063d056dafec7a7c176adddcd034f6d2a3
rmxlinux <dandinking@buaa.edu.cn>
2025-09-22 00:05:47 +0800

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+class pwd:
+    def content(self):
+        return b'' #base64
+    def api(self):
+        return b'' #base64, apikey
+
+'''
+import base64
+print(base64.b64encode(''.encode('utf-8')))
+'''
