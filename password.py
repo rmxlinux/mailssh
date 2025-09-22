e63b9f8c4e1af4f82c68f24a5ace9016a029c40e
rmxlinux <dandinking@buaa.edu.cn>
2025-09-22 11:01:42 +0800

    Add files via upload

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 class pwd:
     def content(self):
-        return b'' #email password
+        return b'' #email password b64
     def api(self):
-        return b'' #api key
+        return b'' #api key b64
     def imap(self):
         return "imap.126.com", 993, "remote_test@126.com" #imap server, your address
     def smtp(self):
         return "smtp.126.com", 465, "remote_test@126.com" #smtp server, your address
     def id(self):
         return {'name': '', 'version': '1.0', 'vendor': ''} #mail app id
+    def console(self):
+        return b'' #consolemode password b64
 
 '''
 import base64
